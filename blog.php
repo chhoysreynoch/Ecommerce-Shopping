a7aed7f700acb3438cb17d93b1fb80c788631f1f
Chhoy Sreynoch <chhoysreynoch@gmail.com>
2025-02-14 14:50:46 +0700

    add new page blog

---
+++ blog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "includes/head.php" ?>
+<body>
+
+    <!-- Header -->
+    <div class="header-v4">
+        <?php include "includes/blogHeader.php" ?>
+    </div>
+    
+    <!-- Title page -->
+	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-02.jpg');">
+		<h2 class="ltext-105 cl0 txt-center">
+			Blog
+		</h2>
+	</section>	
+
+    <!-- Content page -->
+    <?php include "includes/containBlog.php" ?> 
+
+    <!-- Footer -->
+     <?php include "includes/footer.php" ?>
+
+</body>
+</html>
